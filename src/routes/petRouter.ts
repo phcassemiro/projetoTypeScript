1c7b14ef8b4773212e8d22a0eac9e408b5f613de
Pedro Henrique Cassemiro da Silva <phcassemiro@hotmail.com>
2025-01-31 14:36:05 -0300

    Surgimento de erro no petRouter.ts

---
+++ src/routes/petRouter.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import PetController from "../controller/PetController";
+
+const router  = express.Router();
+
+const petController = new PetController();
+
+router.post("/",petController.criaPet);
+
